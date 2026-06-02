bad8f2bb735c5e1384363937e8b37ce602841b1a
strGT20 <saptalokazone@gmail.com>
2026-06-02 23:36:49 +0700

    Move data class User dari AuthModels -> UserModels

--- app/src/main/java/com/example/mybank/data/models/AuthModels.kt
+++ app/src/main/java/com/example/mybank/data/models/AuthModels.kt
@@ -46,35 +46,3 @@ data class AuthData(
     @SerializedName("user")
     val user: User
 )
-
-data class User(
-    @SerializedName("id")
-    val id: Int,
-    @SerializedName("name")
-    val name: String?,
-    @SerializedName("email")
-    val email: String,
-    @SerializedName("phone")
-    val phone: String?,
-    @SerializedName("date_of_birth")
-    val dateOfBirth: String?, // Diberi tanda '?' (nullable) untuk jaga-jaga jika BE mengirim null
-    @SerializedName("occupation")
-    val occupation: String?,
-    @SerializedName("segment")
-    val segment: String?,
-    @SerializedName("is_active")
-    val isActive: Boolean,
-    @SerializedName("is_personalization_enabled")
-    val isPersonalizationEnabled: Boolean,
-    @SerializedName("created_at")
-    val createdAt: String
-)
-
-data class Meta(
-    @SerializedName("limit")
-    val limit: Int,
-    @SerializedName("page")
-    val page: Int,
-    @SerializedName("total_items")
-    val totalItems: Int
-)
